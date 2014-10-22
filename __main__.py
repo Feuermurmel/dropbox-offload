9fd71537ef58d1d503aa25558d40cc941b984d03
Michael Schwarz <michi.schwarz@gmail.com>
2014-10-22 22:36:28 +0200

    Removed directory watching functionality.

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import sys, os, shutil, re, subprocess, argparse, contextlib, threading
+import sys, os, shutil, re, argparse
 
 
 class UserError(Exception):
@@ -10,18 +10,6 @@ class UserError(Exception):
 
 def log(msg, *args):
 	print(msg.format(*args), file = sys.stderr)
-
-
-def command(args):
-	process = subprocess.Popen(args, stdout = subprocess.PIPE)
-	process.communicate()
-	
-	assert not process.returncode
-
-
-def read_file(path):
-	with open(path, 'rb') as file:
-		return file.read()
 
 
 def numeric_sort_key(str):
@@ -82,47 +70,9 @@ def rename(source, target):
 	os.rename(source, target)
 
 
-def start_daemon_thread(target):
-	thread = threading.Thread(target = target)
-	
-	thread.setDaemon(True)
-	thread.start()
-
-
-@contextlib.contextmanager
-def dir_watcher_event(dir):
-	if sys.platform == 'darwin':
-		args = ['fsevents', '--bare', dir]
-	elif sys.platform.startswith('linux'):
-		args = ['inotifywait', '-rm', '-e', 'close_write,move,create,delete', '--format', '.', dir]
-	else:
-		raise UserError('Unknown platform: {}', sys.platform)
-	
-	process = subprocess.Popen(args, stdout = subprocess.PIPE)
-	event = threading.Event()
-	
-	def target():
-		while True:
-			process.stdout.readline()
-			event.set()
-
-	start_daemon_thread(target)
-	
-	try:
-		def fn():
-			event.wait()
-			event.clear()
-		
-		yield fn
-	finally:
-		process.kill()
-		process.wait()
-
-
 def parse_args():
 	parser = argparse.ArgumentParser()
 	
-	parser.add_argument('-c', '--continuous', action = 'store_true', default = False)
 	parser.add_argument('-n', '--count', type = int, default = 3)
 	parser.add_argument('queue_dir')
 	parser.add_argument('offload_dir')
@@ -148,21 +98,15 @@ class Statistics:
 
 def process_directories(offload_dir, queue_dir, count):
 	top_level_dir_names = set(iter_child_dirs(queue_dir)) | set(iter_child_dirs(offload_dir))
-	queue_file_count = 0
-	offload_file_count = 0
 	
 	for top_level_dir in sorted(top_level_dir_names, key = numeric_sort_key):
 		queue_top_level_dir = os.path.join(queue_dir, top_level_dir)
 		offload_top_level_dir = os.path.join(offload_dir, top_level_dir)
-
-		files = sorted(set(iter_files(queue_top_level_dir)) | set(
-			iter_files(offload_top_level_dir)), key = numeric_sort_key)
-
+		
+		files = sorted(set(iter_files(queue_top_level_dir)) | set(iter_files(offload_top_level_dir)), key = numeric_sort_key)
+		
 		queue_files = files[:count]
 		offload_files = files[count:]
-		
-		queue_file_count += len(queue_files)
-		offload_file_count += len(offload_files)
 		
 		for i in queue_files:
 			queue_path = os.path.join(queue_top_level_dir, i)
@@ -176,7 +120,7 @@ def process_directories(offload_dir, queue_dir, count):
 					
 					rename(offload_path, queue_path)
 					remove_empty_parent(offload_path, offload_top_level_dir)
-
+		
 		for i in offload_files:
 			queue_path = os.path.join(queue_top_level_dir, i)
 			offload_path = os.path.join(offload_top_level_dir, i)
@@ -188,32 +132,12 @@ def process_directories(offload_dir, queue_dir, count):
 				log('Offloading: {}', os.path.join(top_level_dir, i))
 				
 				rename(queue_path, offload_path)
-	
-	return Statistics(queue_file_count, offload_file_count)
 
 
 def main():
 	args = parse_args()
-
-	queue_dir = args.queue_dir
-	offload_dir = args.offload_dir
-	count = args.count
 	
-	if args.continuous:
-		stat = None
-		
-		with dir_watcher_event(queue_dir) as watcher:
-			while True:
-				new_stat = process_directories(offload_dir, queue_dir, count)
-				
-				if new_stat != stat:
-					stat = new_stat
-					stat.log()
-				
-				watcher()
-	else:
-		stat = process_directories(offload_dir, queue_dir, count)
-		stat.log()
+	process_directories(args.offload_dir, args.queue_dir, args.count)
 
 
 try:
