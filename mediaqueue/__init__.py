cbd944e537ba39170aaf96596f3683c3b22f270b
Michael Schwarz <michi.schwarz@gmail.com>
2017-09-19 14:12:18 +0200

    Use `active` and `offload` nomenclature in code

--- mediaqueue/__init__.py
+++ mediaqueue/__init__.py
@@ -115,39 +115,39 @@ def parse_args():
         '--per-directory-limit',
         type=int,
         default=math.inf,
-        help='Maximum number of files to put into the queue per top-level '
-             'directory. By default no limit applies, unless no limit is set '
-             'for both --global-limit and --size-limit, in which case is '
-             'defaults to 3.')
+        help='Maximum number of files to put into the active dir per '
+             'top-level directory. By default no limit applies, unless no '
+             'limit is set for both --global-limit and --size-limit, in which '
+             'case is defaults to 3.')
 
     parser.add_argument(
         '-N',
         '--global-limit',
         type=int,
         default=math.inf,
-        help='Maximum number of files to put into the queue. By default no '
-             'limit applies.')
+        help='Maximum number of files to put into the active dir. By default '
+             'no limit applies.')
 
     parser.add_argument(
         '-m',
         '--global-minimum',
         type=int,
         default=1,
-        help='Minimum number of files to put into the queue. This defaults to '
-             'one.')
+        help='Minimum number of files to put into the active dir. This '
+             'defaults to one.')
 
     parser.add_argument(
         '-s',
         '--size-limit',
         type=size_arg,
         default=math.inf,
-        help='Maximum number combined file size to put into the queue as '
+        help='Maximum number combined file size to put into the active dir as '
              'number of bytes. You can use suffixes k, m ... y for powers of '
              '1000 and K, M ... for powers of 1024. By default no limit '
              'applies.')
 
     parser.add_argument(
-        'queue_dir',
+        'active_dir',
         help='The directory into which the active files should be put.')
 
     parser.add_argument(
@@ -156,10 +156,10 @@ def parse_args():
 
     args = parser.parse_args()
 
-    if is_subdir_of(args.queue_dir, args.offload_dir) or is_subdir_of(args.offload_dir, args.queue_dir):
+    if is_subdir_of(args.active_dir, args.offload_dir) or is_subdir_of(args.offload_dir, args.active_dir):
         raise UserError(
-            'The queue and offload dirs may not contain each other or be the '
-            'same directory.')
+            'The active dir and offload dirs may not contain each other or be '
+            'the same directory.')
 
     if args.per_directory_limit == math.inf and args.global_limit == math.inf and args.size_limit == math.inf:
         args.per_directory_limit = 3
@@ -167,9 +167,9 @@ def parse_args():
     return args
 
 
-def collect_files_with_sizes(queue_dir, offload_dir):
+def collect_files_with_sizes(active_dir, offload_dir):
     size_by_file_by_dir = collections.defaultdict(dict)
-    for i in [queue_dir, offload_dir]:
+    for i in [active_dir, offload_dir]:
         for j in iter_child_dirs(i):
             for k in iter_files(os.path.join(i, j)):
                 size_by_file_by_dir[j][k] = get_size(os.path.join(i, j, k))
@@ -178,10 +178,10 @@ def collect_files_with_sizes(queue_dir, offload_dir):
 
 def select_files(size_by_file_by_dir, per_directory_limit, global_limit, size_limit, global_minimum):
     """
-    Return a list of tuples (queue, dir, file) for each file where queue is a
-    boolean telling whether the file should be queued instead of offloaded,
-    dir is the top-level-directory name in which the file is and file is the
-    path relative to the top-level directory.
+    Return a list of tuples (activate, dir, file) for each file where activate
+    is a boolean telling whether the file should be activated instead of
+    offloaded, dir is the top-level-directory name in which the file is and
+    file is the path relative to the top-level directory.
     """
     def key(x):
         dir, (index, file) = x
@@ -211,17 +211,17 @@ def select_files(size_by_file_by_dir, per_directory_limit, global_limit, size_li
 
         for dir, (_, file) in ordered_files:
             size = size_by_file_by_dir[dir][file]
-            queue = (
+            activate = (
                 num_files < global_minimum
                 or (
                     num_files_by_directory[dir] < per_directory_limit
                     and num_files < global_limit
                     and size <= size_limit - num_bytes)) \
-                and dir not in capped_directories
+                       and dir not in capped_directories
 
-            yield queue, dir, file
+            yield activate, dir, file
 
-            if queue:
+            if activate:
                 num_files_by_directory[dir] += 1
                 num_files += 1
                 num_bytes += size
@@ -231,8 +231,8 @@ def select_files(size_by_file_by_dir, per_directory_limit, global_limit, size_li
     return list(iter_files())
 
 
-def main(queue_dir, offload_dir, per_directory_limit, global_limit, size_limit, global_minimum):
-    size_by_file_by_dir = collect_files_with_sizes(queue_dir, offload_dir)
+def main(active_dir, offload_dir, per_directory_limit, global_limit, size_limit, global_minimum):
+    size_by_file_by_dir = collect_files_with_sizes(active_dir, offload_dir)
 
     files = select_files(
         size_by_file_by_dir,
@@ -241,28 +241,28 @@ def main(queue_dir, offload_dir, per_directory_limit, global_limit, size_limit, 
         size_limit,
         global_minimum)
 
-    for queue, dir, file in files:
-        queue_path = os.path.join(queue_dir, dir, file)
+    for activate, dir, file in files:
+        active_path = os.path.join(active_dir, dir, file)
         offload_path = os.path.join(offload_dir, dir, file)
 
-        if queue:
+        if activate:
             if os.path.exists(offload_path):
-                if os.path.exists(queue_path):
+                if os.path.exists(active_path):
                     remove(offload_path, offload_dir)
                 else:
                     log('Activating: {}', os.path.join(dir, file))
 
-                    rename(offload_path, queue_path)
-                    remove_empty_parents(offload_path, os.path.join(queue_dir, dir))
+                    rename(offload_path, active_path)
+                    remove_empty_parents(offload_path, os.path.join(active_dir, dir))
         else:
-            if os.path.exists(queue_path):
+            if os.path.exists(active_path):
                 if os.path.exists(offload_path):
                     remove(offload_path, offload_dir)
 
                 path = os.path.join(dir, file)
                 log('Offloading: {}', path)
 
-                rename(queue_path, offload_path)
+                rename(active_path, offload_path)
 
 
 def script_main():
